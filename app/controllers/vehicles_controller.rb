07921679a9e3f631ccf129ae2183d42a7519256a
hbnoboa <hbnoboa11@gmail.com>
2024-03-25 08:42:06 -0300

    main

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -3,21 +3,42 @@ class VehiclesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: %i[create update]
 
   def index
-    set_vehicles
+    @ship = params[:ship]
+    @chassis = params[:chassis]
+    @model = params[:model]
+    @situation = params[:situation]
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
+    ship_and_travel = params[:ship_and_travel]
+    if ship_and_travel.present?
+      @ship, @travel = ship_and_travel.split('-').map(&:strip)
+    end
+  
+    @vehicles = Vehicle.where(done: 'yes').where(:updated_at.ne => nil)
+  
+    @vehicles = @vehicles.where(ship: @ship) if @ship.present?
+    @vehicles = @vehicles.where(travel: @travel) if @travel.present?
+    @vehicles = @vehicles.where(:chassis => /#{@chassis}/i) if @chassis.present?
+    @vehicles = @vehicles.where(model: /#{@model}/i) if @model.present?
+    @vehicles = @vehicles.where(situation: /#{@situation}/i) if @situation.present?
+    @vehicles = @vehicles.where(updated_at: @start_date..@end_date) if @start_date.present? && @end_date.present?
+  
+    @vehicles = @vehicles.order(updated_at: :asc)
+  
     page = (params[:page] || 1).to_i
     per_page = 10
-
+  
     total_count = @vehicles.count
     total_pages = (total_count.to_f / per_page).ceil
-
+  
     offset = per_page * (page - 1)
     limit = per_page
-
+  
     @vehicles = @vehicles.limit(limit).skip(offset)
-
+  
     @pagy = Pagy.new(count: total_count, page: page, items: per_page)
-
   end
+  
 
   def show
   end
@@ -38,11 +59,11 @@ class VehiclesController < ApplicationController
     pdf.stroke_horizontal_rule
 
   
-    
+  
     pdf.bounding_box([pdf.bounds.left + 5, pdf.cursor - 15], width: pdf.bounds.width) do
-      pdf.text_box "Chassi: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top], size: 10
+      pdf.text_box "Chassi: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top]
       pdf.text_box @vehicle.chassis, at: [pdf.bounds.left + 40, pdf.bounds.top], size: 10, style: :bold   
-      pdf.text_box "Modelo: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 15], size: 10
+      pdf.text_box "Modelo: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 15]
       pdf.text_box @vehicle.model, at: [pdf.bounds.left + 40, pdf.bounds.top - 15], size: 10, style: :bold   
     end
     pdf.move_down 50
@@ -157,11 +178,24 @@ class VehiclesController < ApplicationController
              )
   end
 
-  def index_pdf
-    set_vehicles
-
-    @nc = Vehicle.all.where(done: 'yes', :updated_at.ne => nil).gt(nonconformity: 0).order(updated_at: :asc)
+    def index_pdf
+    @ship = params[:ship]
+    @chassis = params[:chassis]
+    @model = params[:model]
+    @situation = params[:situation]
+    @start_date = params[:start_date]
+    @end_date = params[:end_date]
   
+    @vehicles = Vehicle.where(done: 'yes').where(:updated_at.ne => nil)
+  
+    @vehicles = @vehicles.where(ship: @ship) if @ship.present?
+    @vehicles = @vehicles.where(:chassis => /#{@chassis}/i) if @chassis.present?
+    @vehicles = @vehicles.where(model: /#{@model}/i) if @model.present?
+    @vehicles = @vehicles.where(situation: /#{@situation}/i) if @situation.present?
+    @vehicles = @vehicles.where(updated_at: @start_date..@end_date) if @start_date.present? && @end_date.present?
+  
+    @vehicles = @vehicles.order(updated_at: :asc)
+    
     pdf = Prawn::Document.new(page_size: 'A4', :margin => [70,70,70,70])
   
     image_width = 80
@@ -178,23 +212,23 @@ class VehiclesController < ApplicationController
       pdf.text "RELATÓRIO DE INSPEÇÃO", size: 10
       pdf.move_down 40
       pdf.text "1 – INFORMAÇÃO GERAL" , size: 10, align: :center, style: :bold   
-      pdf.text_box "NOME DO NAVIO: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 70], size: 10, style: :bold  
-      pdf.text_box @vehicles.first.ship, at: [pdf.bounds.left + 90, pdf.bounds.top - 70], size: 10
-      pdf.text_box "Nº DA VIAGEM: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 90], size: 10, style: :bold  
-      pdf.text_box @vehicles.first.travel, at: [pdf.bounds.left + 75, pdf.bounds.top - 90], size: 10
-      pdf.text_box "DATA DA OPERAÇÃO: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 110], size: 10, style: :bold  
-      pdf.text_box @vehicles.first.updated_at.strftime("%d/%m/%Y"), at: [pdf.bounds.left + 110, pdf.bounds.top - 110], size: 10
+      pdf.text_box "NOME DO NAVIO: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 70], style: :bold  
+      pdf.text_box @vehicles.first.ship, at: [pdf.bounds.left + 90, pdf.bounds.top - 70]
+      pdf.text_box "Nº DA VIAGEM: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 90], style: :bold  
+      pdf.text_box @vehicles.first.travel, at: [pdf.bounds.left + 75, pdf.bounds.top - 90]
+      pdf.text_box "DATA DA OPERAÇÃO: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 110], style: :bold  
+      pdf.text_box @vehicles.first.updated_at.strftime("%d/%m/%Y"), at: [pdf.bounds.left + 110, pdf.bounds.top - 110]
     end
     pdf.bounding_box([pdf.bounds.left, pdf.cursor, pdf.bounds.bottom], width: pdf.bounds.width, height: 100) do
       pdf.text "2 – INFORMAÇÕES DA OPERAÇÃO " , size: 10, align: :center, style: :bold 
-      pdf.text_box "INICIO OPERAÇÃO / DESCARGA: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 20], size: 10, style: :bold  
-      pdf.text_box  @vehicles.first.updated_at.strftime("%d/%m/%Y - %H:%M"), at: [pdf.bounds.left + 165, pdf.bounds.top - 20], size: 10
-      pdf.text_box "FIM OPERAÇÃO DESCARGA: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 40], size: 10, style: :bold  
-      pdf.text_box  @vehicles.last.updated_at.strftime("%d/%m/%Y - %H:%M"), at: [pdf.bounds.left + 145, pdf.bounds.top - 40], size: 10
-      pdf.text_box "QUANTIDADE DE VEÍCULOS: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 60], size: 10, style: :bold  
-      pdf.text_box  "#{@vehicles.count.to_s} VEÍCULOS", at: [pdf.bounds.left + 146, pdf.bounds.top - 60], size: 10
-      pdf.text_box "VEÍCULOS AVARIADOS: ", size: 10, at: [pdf.bounds.left + 250, pdf.bounds.top - 60], size: 10, style: :bold  
-      pdf.text_box "#{@vehicles.where(:nonconformity.gt => 0).count} VEÍCULO(S)", at: [pdf.bounds.left + 370, pdf.bounds.top - 60], size: 10
+      pdf.text_box "INICIO OPERAÇÃO / DESCARGA: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 20], style: :bold  
+      pdf.text_box  @vehicles.first.updated_at.strftime("%d/%m/%Y - %H:%M"), at: [pdf.bounds.left + 165, pdf.bounds.top - 20]
+      pdf.text_box "FIM OPERAÇÃO DESCARGA: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 40], style: :bold  
+      pdf.text_box  @vehicles.last.updated_at.strftime("%d/%m/%Y - %H:%M"), at: [pdf.bounds.left + 145, pdf.bounds.top - 40]
+      pdf.text_box "QUANTIDADE DE VEÍCULOS: ", size: 10, at: [pdf.bounds.left, pdf.bounds.top - 60], style: :bold  
+      pdf.text_box  "#{@vehicles.count.to_s} VEÍCULOS", at: [pdf.bounds.left + 146, pdf.bounds.top - 60]
+      pdf.text_box "VEÍCULOS AVARIADOS: ", size: 10, at: [pdf.bounds.left + 250, pdf.bounds.top - 60], style: :bold  
+      pdf.text_box "#{@vehicles.where(:nonconformity.gt => 0).count} VEÍCULO(S)", at: [pdf.bounds.left + 370, pdf.bounds.top - 60]
     end
 
     header = [["MARCA", "MODELO", "Unidades", "Total Avariados", "%"]]
@@ -257,7 +291,7 @@ class VehiclesController < ApplicationController
 
     errors = []
 
-    Vehicle.all.where(done: 'yes', :updated_at.ne => nil).gt(nonconformity: 0).order(updated_at: :asc).each do |vehicle|
+    @vehicles.where.gt(nonconformity: 0).order(updated_at: :asc).each do |vehicle|
       vehicle.nonconformities.each_with_index do |nonconformity, index|
         begin
           chassi = StringIO.new(Base64.decode64(vehicle.etChassisImage.sub('data:image/jpeg;base64,', '')))
@@ -266,7 +300,7 @@ class VehiclesController < ApplicationController
           image2 = StringIO.new(Base64.decode64(nonconformity.image2.sub('data:image/jpeg;base64,', '')))
         rescue StandardError => e
           errors << "Error decoding image for vehicle #{vehicle.chassis}: #{e.message}"
-          next # Move to the next vehicle if an error occurs
+          next
         end
         
         row = [
@@ -277,24 +311,17 @@ class VehiclesController < ApplicationController
         ]
         photos << row
     
-        vehicle_row = [vehicle.chassis, vehicle.model, nonconformity.vehicleParts, nonconformity.nonconformityTypes]
+        vehicle_row = [vehicle.chassis, vehicle.model, VehiclePart.find(nonconformity.vehicleParts).name, NonconformityType.find(nonconformity.nonconformityTypes).nctype]
         photos << vehicle_row
       end
     end
     
-    # Print errors after the loop ends
-    errors.each { |error| puts error }
-    
-    begin
-      pdf.table(headerPhotos + photos, cell_style: { inline_format: true }) do |table|
-        table.header = 1
-        table.cells.style(size: 6, align: :center)
-        table.column_widths = [112.5, 112.5, 112.5, 112.5]
-        table.cells.style(border_color: 'FFFFFF', border_width: 0)
-        table.cells.style(size: 6, align: :center)
-      end
-    rescue StandardError => e
-      puts "Error in pdf.table block: #{e.message}"
+    pdf.table(headerPhotos + photos, cell_style: { inline_format: true }) do |table|
+      table.header = 1
+      table.cells.style(size: 6, align: :center)
+      table.column_widths = [112.5, 112.5, 112.5, 112.5]
+      table.cells.style(border_color: 'FFFFFF', border_width: 0)
+      table.cells.style(size: 6, align: :center)
     end
 
   end
