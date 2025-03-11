3740e6a91ff2a58f633ea2b06247f10551ce057c
hbnoboa <hbnoboa11@gmail.com>
2025-03-11 13:06:44 -0300

    main

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'vehicles/all', to: 'vehicles#all_vehicles'
   get 'vehicles/show_pdf/:id', to: 'vehicles#show_pdf', as: 'vehicles_show_pdf'
   get 'home', to: 'home#index'
   root 'home#index'
