e55cdf8f1e0c01b1eeffb8f300e98ff875af0789
hbnoboa <hbnoboa11@gmail.com>
2025-03-31 12:18:58 -0300

    main

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,6 +41,11 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = "wss://example.com/cable"
   # config.action_cable.allowed_request_origins = [ "http://example.com", /http:\/\/example.*/ ]
+  config.action_cable.url = "wss://ios-car-f8bf78f8eb62.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = [
+    "https://ios-car-f8bf78f8eb62.herokuapp.com",
+    "http://ios-car-f8bf78f8eb62.herokuapp.com"
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
