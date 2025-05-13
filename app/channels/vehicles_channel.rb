dadec9b32bbf2edeb69e61fbbfa7e6cc176f3430
hbnoboa <hbnoboa11@gmail.com>
2025-05-13 11:02:50 -0300

    broadcast_vehicles_list

--- app/channels/vehicles_channel.rb
+++ app/channels/vehicles_channel.rb
@@ -2,10 +2,21 @@ class VehiclesChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
     stream_from "vehicles_channel"
+    broadcast_vehicles_list
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  private
+
+  def broadcast_vehicles_list
+    vehicles = Vehicle.where(done: 'yes').where(:updated_at.ne => nil).order(updated_at: :desc)
+    ActionCable.server.broadcast("vehicles_channel", {
+      action: "index",
+      vehicles: vehicles.as_json
+    })
   end
 end
   
