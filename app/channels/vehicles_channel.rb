93866f5167ee099cba35cc291278e7bdb7c90067
hbnoboa <hbnoboa11@gmail.com>
2025-05-14 14:24:12 -0300

    main

--- app/channels/vehicles_channel.rb
+++ app/channels/vehicles_channel.rb
@@ -10,10 +10,9 @@ class VehiclesChannel < ApplicationCable::Channel
   private
 
   def broadcast_vehicles_list
-    vehicles = Vehicle.where(done: 'yes')
     ActionCable.server.broadcast("vehicles_channel", {
       action: "index",
-      vehicles: vehicles.as_json
+      vehicles: Vehicle.all.as_json
     })
   end
 end
